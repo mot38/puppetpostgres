b4ab4df21c2f4445602c2e111c9d50eb7e5fbf94
lezlightlr <lezlightlr@gmail.com>
2015-12-15 16:14:13 +0000

    backup restore script still wip

--- backup_restore.bash
+++ backup_restore.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 data_dir=/var/lib/pgsql/9.4/data/
 recovery_conf=${data_dir}recovery.conf
+export cluster_show_cmd='/usr/pgsql-9.4/bin/repmgr -f /etc/repmgr/9.4/repmgr.conf cluster show'
+
+function master_node_name {
+  $cluster_show_cmd | grep master | sed 's/^.*host=\(node[abc]\) user.*$/\1/'
+}
+
 
 case `hostname` in
   nodea) nodenum=1 ;;
@@ -9,27 +15,31 @@ case `hostname` in
 esac
 
 clear
-echo -e "\n\n\t\tShowing the current status of the cluster, with 'nodea' as the master.\n\n"
-cluster_show_cmd='/usr/pgsql-9.4/bin/repmgr -f /etc/repmgr/9.4/repmgr.conf cluster show'
 
-$cluster_show_cmd
+echo -e "\n\n\tShowing the current status of the cluster, with '"`master_node_name`"' as the master.\n\n"
+
+tput cuf 10 ;  $cluster_show_cmd
 
 sleep 5
 
-echo -e "\n\n\t\tStopping the Postgres service."
+echo -e "\n\n\tStopping the Postgres service."
 systemctl stop postgresql-9.4
 systemctl status postgresql-9.4
-echo -e "\n\n\t\tThe Postgres service has stopped.\n\n"
-echo -e "\n\n\t\tDeleting everything in the data directory.\n\n"
+echo -e "\n\n\tThe Postgres service has stopped.\n\n"
+echo -e "\n\n\tDeleting everything in the data directory.\n\n"
 rm -rf ${data_dir}*
 
 ls -l ${data_dir}*
-echo -e "\n\n\t\tSee, it's empty!"
+echo -e "\n\n\tSee, it's empty!"
 
 sleep 10
-
 clear
-echo -e "\n\n\t\tNow restore from backup on bart server and press any key to continue with recovery process."
+echo -en "\n\n\tLet's take a backup of the 'Master' node on the 'bart' server; press any key to initiate the backup."
+read x
+su - postgres -c "ssh barman@bart \"barman backup primary\""
+sleep 10
+clear
+echo -en "\n\n\tNow we'll restore from that backup on 'bart' server to `hostname`; press any key to continue with recovery process."
 read x
 clear
 su - postgres -c "ssh barman@bart \"barman recover --remote-ssh-command 'ssh postgres@`hostname`' primary latest /var/lib/pgsql/9.4/data/\""
@@ -43,10 +53,10 @@ HERE
 chmod 644 ${recovery_conf}
 chown postgres:postgres ${recovery_conf}
 su - postgres -c "psql -h vip -U repmgr -c \"select pg_create_physical_replication_slot('repmgr_slot_${nodenum}');\""
-echo -e "\n\n\t\tRe-starting the Postgres service"
+echo -e "\n\n\tRe-starting the Postgres service"
 systemctl start postgresql-9.4
 echo -e "\n\n"
 systemctl status postgresql-9.4
 echo -e "\n\n"
 $cluster_show_cmd
-echo -e "\n\n\t\tThe restored node has now re-joined the cluster and is following the new Master.\n\n"
+echo -e "\n\n\tThe restored '`hostname`' has now re-joined the cluster and is following the new Master '"`master_node_name`"'.\n\n"
